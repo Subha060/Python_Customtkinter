ce36e0cb8bd01638bb1f9802e0f50e98ee988960
Subha Adak <shubhaadak0@gmail.com>
2024-02-02 23:40:01 +0530

    added some new features

--- Login_System.py
+++ Login_System.py
@@ -1,32 +1,59 @@
+# This is a simple login system using customtkinter
 import customtkinter
+from tkinter import messagebox
 
-customtkinter.set_appearance_mode("system")
+def login():
+    email = email_input.get()
+    password = password_input.get()
+    print(email, password)
+    file = open("details.txt", "a")
+    file.write("Email: " + email + "\n" + "Password: " + password + "\n" + "\n")
+    file.close()
+    if email == "" and password == "":
+        messagebox.showerror('login', 'Error')
+    else:
+        messagebox.showinfo('Login', 'Successful')
+
+    email_input.delete(0, 'end')
+    password_input.delete(0, 'end')
+
+
+customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 
 root = customtkinter.CTk()
 
 root.title("Login System")
-root.minsize(300,150)
-root.maxsize(500,350)
+# root.iconbitmap("secure_login_icon_150316.ico")
+root.minsize(300, 350)
+root.maxsize(300, 350)
+# root.config(background= "#1a1919")
 
-frame1 = customtkinter.CTkFrame(master=root,width=200,height=50, corner_radius= 15,)
-frame1.pack(pady= 5, padx= 4)
-frame2 = customtkinter.CTkFrame(master=root,width=200,height=50,corner_radius=15)
-frame2.pack(pady= 25, padx= 4)
+frame1 = customtkinter.CTkFrame(master=root, width=300, height=50, corner_radius=15, fg_color="#1a1919")
+frame1.pack(padx=5, pady=(10, 5))
+frame2 = customtkinter.CTkFrame(master=root, width=200, height=50, corner_radius=15, fg_color="#1a1919")
+frame2.pack(pady=10, padx=4)
 
-label1 = customtkinter.CTkLabel(master=frame1,text="Login System",font=("Sans serif",30),text_color="green")
-label1.pack(pady= 5, padx= 4)
+login_text = customtkinter.CTkLabel(master=frame1, text="Login System", text_color="#b8f0f2", font=("Algerian", 25))
+login_text.pack(pady=(10, 12), padx=17)
 
-username = customtkinter.CTkEntry(master=frame2,placeholder_text="Username")
-username.pack(pady= 15, padx= 24)
-password = customtkinter.CTkEntry(master=frame2,placeholder_text="Password",show= "*")
-password.pack(pady= 15, padx= 24)
+username_label = customtkinter.CTkLabel(master=frame2, text="Username", font=("Baskerville Old Face", 15))
+username_label.pack(pady=(10, 0), padx=(0, 100))
+email_input = customtkinter.CTkEntry(master=frame2, width=170, placeholder_text="Enter Username",
+                                     font=("Baskerville Old Face", 15))
+email_input.pack(pady=(0, 15), padx=24)
 
-checkbox = customtkinter.CTkCheckBox(master=frame2,text="Remember Me")
-checkbox.pack(pady= 20, padx= 10)
+password_label = customtkinter.CTkLabel(master=frame2, text="Password", font=("Baskerville Old Face", 15))
+password_label.pack(pady=(2, 2), padx=(0, 100))
+password_input = customtkinter.CTkEntry(master=frame2, width=170, placeholder_text="Enter Password", show="*",
+                                        font=("Baskerville Old Face", 15))
+password_input.pack(pady=(0, 17), padx=24)
 
-button = customtkinter.CTkButton(master=frame2,text="Submit")
-button.pack(pady= 10, padx= 20)
+checkbox = customtkinter.CTkCheckBox(master=frame2, text="Remember Me", font=("Baskerville Old Face", 16))
+checkbox.pack(pady=0, padx=(23, 60))
 
+button = customtkinter.CTkButton(master=frame2, width=170, text="Submit", corner_radius=12,
+                                 font=("Baskerville Old Face", 20), command=login)
+button.pack(pady=(12, 20), padx=20)
 
 root.mainloop()
